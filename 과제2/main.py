d1913dd786b02ba018adc8b2b7ff936f561dca38
docane <alsgusrl06@gmail.com>
2023-07-11 17:56:30 +0900

    2차 과제

---
+++ 과제2/main.py
@@ -0,0 +1,15 @@
+# 민현기가 짠 코드
+import simplesundragon
+
+if __name__ == '__main__':
+    print('gcd 함수')
+    print(simplesundragon.gcd(72, 54))
+
+    print('\nmultiply_inverse 함수')
+    print(simplesundragon.multiply_inverse(4))
+
+    print('\nhill_num 함수')
+    print(simplesundragon.hill_num('c'))
+
+    print('\nsimple_gcd 함수')
+    print(simplesundragon.simple_gcd(45, 27))
